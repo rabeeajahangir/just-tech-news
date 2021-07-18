771382f9d5f517138707b7f3de628299128f61d7
Rabeea Jahangir <rabeeajahangir@Rabeeas-MacBook-Air.local>
2021-07-17 20:26:33 -0400

    added post model

--- models/index.js
+++ models/index.js
@@ -1,4 +1,14 @@
 const User = require('./User');
 const Post = require('./Post');
 
+
+// create associations (a user can make many posts but a post belongs to one user only)
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+
+  Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+
 module.exports = { User, Post };
